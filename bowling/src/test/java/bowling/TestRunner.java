715b02481fcbf0a37badbcb080bcdcf821e3cd66
Marius Bozem <bozem@wamd.de>
2013-06-27 15:15:33 +0200

    removed tutorial code, added bowling game files

--- bowling/src/test/java/bowling/TestRunner.java
+++ bowling/src/test/java/bowling/TestRunner.java
@@ -1,8 +1,8 @@
-package se.waymark.rentit;
+package bowling;
 
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-public class RunCukesTest {
+public class TestRunner {
 }
