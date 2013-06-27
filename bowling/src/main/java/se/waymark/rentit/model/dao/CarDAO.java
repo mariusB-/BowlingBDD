f4508752415c7a81635e5f36a8f4acd05dc95842
Marius Bozem <bozem@wamd.de>
2013-06-27 14:54:48 +0200

    init

---
+++ bowling/src/main/java/se/waymark/rentit/model/dao/CarDAO.java
@@ -0,0 +1,12 @@
+package se.waymark.rentit.model.dao;
+
+import se.waymark.rentit.model.entiy.Car;
+
+public interface CarDAO {
+
+    public void add(Car car);
+
+    Car findAvailableCar();
+
+    int getNumberOfAvailableCars();
+}
---
+++ bowling/src/main/java/se/waymark/rentit/model/entiy/Car.java
@@ -0,0 +1,13 @@
+package se.waymark.rentit.model.entiy;
+
+public class Car {
+    private boolean rented;
+
+    public void rent() {
+        rented = true;
+    }
+
+    public boolean isRented() {
+        return rented;
+    }
+}
